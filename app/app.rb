da9ecbf8e7b7a395b52c586ac6f5d1474920b636
jaequery <jaequery@gmail.com>
2015-10-29 20:46:48 -0700

    updated examples

--- app/app.rb
+++ app/app.rb
@@ -17,8 +17,8 @@ module Honeybadger
 
     ### single sign on ###
     use OmniAuth::Builder do
-      provider :twitter,  'kNDElilnZeGQwhYwci0zrn2V9', '4m00r0eCAEy4MbOQUwb0S1milhavlxMKDrDmE7Bm4g5pkRmkZg'
-      provider :instagram,  '66ccf67d4f094f849063bf94f8959350', 'fd30c164216a4ab8ad17e6ed19ed4122'
+      provider :twitter,  '--key--', '--secret--'
+      provider :instagram,  '--key--', '--secret--'
     end
 
     get '/auth/:name/callback' do
@@ -47,15 +47,7 @@ module Honeybadger
     end
 
     get "/login" do
-
-  <<-HTML
-#{settings.omg}
-  <a href='/auth/twitter'>Sign in with Twitter</a><br>
-  <a href='/auth/instagram'>Sign in with Instagram</a><br>
-  <a href='/auth/facebook'>Sign in with Facebook</a><br>
-  <a href='/auth/github'>Sign in with Github</a><br>
-  <a href='/auth/linkedin'>Sign in with Linkedin</a><br>
-  HTML
+      render "login"
     end
 
   end
