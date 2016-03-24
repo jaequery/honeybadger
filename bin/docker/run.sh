93879ae007ff49211e57c7cfc9677910f4868607
jaequery <jaequery@gmail.com>
2016-03-23 22:29:02 -0700

    removed un-used bin file

--- bin/docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-parentdir="$(dirname "$dir")"
-parentdir="$(dirname "$parentdir")"
-app=${parentdir##*/}
-docker exec -it ${app}_app_1 $1
